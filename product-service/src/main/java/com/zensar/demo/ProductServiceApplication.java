223850d73b4eb6a8c7b10d824efc7f0bc9e225ae
AK9773 <105356204+AK9773@users.noreply.github.com>
2022-06-20 14:59:29 +0530

    Changes in Product Service Application to create war Artifact

--- product-service/src/main/java/com/zensar/demo/ProductServiceApplication.java
+++ product-service/src/main/java/com/zensar/demo/ProductServiceApplication.java
@@ -3,12 +3,14 @@ package com.zensar.demo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "test", "com.zensar" })
-public class ProductServiceApplication {
+public class ProductServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
@@ -20,5 +22,12 @@ public class ProductServiceApplication {
 		return new ModelMapper();
 
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
+	
+	
 
 }
