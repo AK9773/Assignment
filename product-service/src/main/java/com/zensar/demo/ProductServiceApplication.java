37fa1187a4f5bed11ce441d0cf4ccd80cc27d395
AK9773 <prajapatiaman445@gmail.com>
2022-06-23 18:21:53 +0530

    Product Service with Swagger

--- product-service/src/main/java/com/zensar/demo/ProductServiceApplication.java
+++ product-service/src/main/java/com/zensar/demo/ProductServiceApplication.java
@@ -3,14 +3,14 @@ package com.zensar.demo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
+@OpenAPIDefinition
 @ComponentScan(basePackages = { "test", "com.zensar" })
-public class ProductServiceApplication extends SpringBootServletInitializer{
+public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
@@ -22,12 +22,5 @@ public class ProductServiceApplication extends SpringBootServletInitializer{
 		return new ModelMapper();
 
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return super.configure(builder);
-	}
-	
-	
 
 }
