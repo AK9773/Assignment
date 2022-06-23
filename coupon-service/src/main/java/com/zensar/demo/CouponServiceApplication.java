f950e651216be9b21f517a0e6cb85cffad350129
AK9773 <105356204+AK9773@users.noreply.github.com>
2022-06-23 09:54:04 +0530

    Coupon Service with Swagger

--- coupon-service/src/main/java/com/zensar/demo/CouponServiceApplication.java
+++ coupon-service/src/main/java/com/zensar/demo/CouponServiceApplication.java
@@ -3,11 +3,12 @@ package com.zensar.demo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class CouponServiceApplication {
 
 	public static void main(String[] args) {
