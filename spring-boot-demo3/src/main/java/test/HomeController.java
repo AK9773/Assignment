b3941466fb7542eee1259f9d80000fb2fbd2309b
AK9773 <prajapatiaman445@gmail.com>
2022-07-01 08:49:26 +0530

    with spring boot security

--- spring-boot-demo3/src/main/java/test/HomeController.java
+++ spring-boot-demo3/src/main/java/test/HomeController.java
@@ -1,19 +1,26 @@
 package test;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/public")
 public class HomeController {
-
-	@RequestMapping("/")
-	public String sayHello() {
-		return "<h1> Welcome to Spring Boot </h1>";
+	
+	@GetMapping("/home")
+	public String home() {
+		return "This is Home page";
 	}
 	
-	@RequestMapping("/abc")
-	public String sayHello1() {
-		return "<h2> Hello </h2>";
+	@GetMapping("/login")
+	public String login() {
+		return "This is Login page";
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+		return "This is Register page";
 	}
 
 }
