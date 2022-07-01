fdae7d0e716555154869cc3a738c43ec1d2a476a
AK9773 <prajapatiaman445@gmail.com>
2022-07-01 12:00:12 +0530

    updated project with war artifact

--- spring-boot-demo3/src/main/java/com/zensar/demo/controllers/StudentController.java
+++ spring-boot-demo3/src/main/java/com/zensar/demo/controllers/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -52,6 +53,7 @@ public class StudentController {
 
 	// @RequestMapping(value = "/students", method = RequestMethod.POST)
 	@PostMapping(value = "/students")
+	//@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<StudentDto> insertStudents(@RequestBody StudentDto studentDto) {
 		return new ResponseEntity<StudentDto>(studentServices.insertStudents(studentDto), HttpStatus.OK);
 	}
--- spring-boot-demo3/src/main/java/com/zensar/demo/security/SpringSecurityConfig.java
+++ spring-boot-demo3/src/main/java/com/zensar/demo/security/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.zensar.demo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,12 +14,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http
+			.csrf().disable()
 			.authorizeRequests()
 			.antMatchers("/public/**").permitAll()
 			.antMatchers("/student-api/**").hasRole("ADMIN")
